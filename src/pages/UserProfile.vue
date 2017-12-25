a6e9d8e86396961fbdb6f24859ea5485544b0c3b
Itsikben <itsikba5@gmail.com>
2017-12-25 20:20:38 +0200

    service edit

--- src/pages/UserProfile.vue
+++ src/pages/UserProfile.vue
@@ -1,20 +1,32 @@
 <template>
   <section>
     <h1>my profile</h1>
+    <div v-if="user">
+      {{user.id}}
+      {{user.userName}}
+    </div>
     <user-details></user-details>
     <user-gallery></user-gallery>
   </section>
 </template>
 
 <script>
+import UserService from '../services/UserService'
 import UserDetails from '../components/UserDetails'
 import UserGallery from '../components/UserGallery'
 export default {
   name: "myProfile",
   data() {
     return {
+      user: null,
       msg: "Welcome to Your Vue.js App"
     };
+  },
+  created(){
+    UserService.getUserById(0).then(u =>{
+       this.user = u;
+       console.log(this.user, u);
+    });
   },
   components:{
     UserDetails,
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,12 +1,36 @@
-
-var users =[
-    {
-        
+var testUser = JSON.parse(`
+{
+    "id": 0,
+    "userName": "itsik",
+    "password": "123456",
+    "userDescriptions": "string",
+    "profilePic": "id of a photo",
+    "postIds": [],
+    "followersIds": [],
+    "followingIds": [],
+    "personalDetails": {
+        "firstName": "Itsik",
+        "lastName": "Ben Arza",
+        "email": "itsik@gmail.com",
+        "address": "bla street 57"
     }
-]
+}
+`)
+var users = [testUser];
 
+function getUsers(){
+    return Promise.resolve(users);
+}
 
+function getUserById(userId){
+    return getUsers().then(users => {
+      var resUser = users.find(u => u.id === userId)
+      if(resUser) return resUser
+      else return false
+    })
+}
 
 export default {
-    
+    getUserById,
+    getUsers
 }
